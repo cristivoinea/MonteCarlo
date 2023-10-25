b0141bd043c9d78f8f1588f2041b73408b638372
cristivoinea <cristivoinea20@yahoo.com>
2023-10-25 17:09:31 +0100

    move save config/result to utilities

--- utilities.py
+++ utilities.py
@@ -20,3 +20,52 @@ def FullStats(data: np.array
         std_vector[i] = np.std(data[:i+1], ddof=1)
 
     return mean_vector, std_vector
+
+
+def SaveConfig(state: str, SWAP_term: str, Ne: np.uint8, Ns: np.uint16,
+               Lx: np.float64, Ly: np.float64, t: np.complex128,
+               step_size: np.float64, region_geometry: str, boundary: np.float64,
+               result: np.array, R: np.array, swap_order: np.array):
+    if SWAP_term == 'sign':
+        np.save(f"{state}_full_{SWAP_term}_real_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.npy",
+                np.real(result))
+        np.save(f"{state}_full_{SWAP_term}_imag_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.npy",
+                np.imag(result))
+
+    else:
+        np.save(f"{state}_full_{SWAP_term}_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.npy",
+                result)
+
+    if SWAP_term == 'sign' or SWAP_term == 'mod':
+        np.save(f"{state}_{SWAP_term}_order_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.npy",
+                swap_order)
+
+    np.save(f"{state}_{SWAP_term}_coords_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.npy",
+            R)
+
+
+def SaveResult(state: str, SWAP_term: str, Ne: np.uint8, Ns: np.uint16,
+               Lx: np.float64, Ly: np.float64, M0: np.uint32, t: np.complex128,
+               step_size: np.float64, region_geometry: str, boundary: np.float64,
+               result: np.array, save_result: np.bool_):
+
+    if save_result:
+        if SWAP_term == 'sign':
+            np.savetxt(f"{state}_{SWAP_term}_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.dat",
+                       np.vstack((Stats(np.real(result[int(M0):])),
+                                  Stats(np.imag(result[int(M0):]))
+                                  ))
+                       )
+        else:
+            np.savetxt(f"{state}_{SWAP_term}_Ne_{Ne}_Ns_{Ns}_t_{np.imag(t):.2f}_{region_geometry}_{boundary/Ly:.3f}_step_{step_size/Lx:.3f}.dat",
+                       Stats(result[int(M0):]))
+
+    else:
+        if SWAP_term == 'sign':
+            mean, var = Stats(np.real(result[int(M0):]))
+            print(f"\nMean (real) = {mean} \n Var (real) = {var}")
+            mean, var = Stats(np.imag(result[int(M0):]))
+            print(f"\nMean (imag) = {mean} \n Var (imag) = {var}")
+        else:
+            mean, var = Stats(result[int(M0):])
+            print(f"\nMean = {mean} \n Var = {var}")
