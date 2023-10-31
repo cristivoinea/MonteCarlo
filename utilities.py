b08eb77812ca5d708f3be54088a55657819c9835
cristivoinea <cristivoinea20@yahoo.com>
2023-10-31 11:48:09 +0000

    entropy utility fn

--- utilities.py
+++ utilities.py
@@ -22,6 +22,28 @@ def FullStats(data: np.array
     return mean_vector, std_vector
 
 
+def GetEntropy(Ne, Ns, M, M0, t, step, region_geometry):
+    Lx = np.sqrt(2*np.pi*Ns/np.imag(t))
+    data = np.loadtxt(
+        f"laughlin_SWAP_Ne_{Ne}_Ns_{Ns}_t_1.00_step_{step:.3f}_{region_geometry}s.dat")
+    boundaries = data[:, 0]*2*np.pi*Lx
+    p = data[:, 1:3]
+    mod = data[:, 3:5]
+    sign = data[:, 5:7]
+
+    S2_p = -np.log(p[:, 0])
+    err_p = np.sqrt(p[:, 1]/(p[:, 0]**2))/np.sqrt(M-M0)
+    S2_mod = -np.log(mod[:, 0])
+    err_mod = np.sqrt(mod[:, 1]/(mod[:, 0]**2))/np.sqrt(M-M0)
+    S2_sign = -np.log(sign[:, 0])
+    err_sign = np.sqrt(sign[:, 1]/(sign[:, 0]**2))/np.sqrt(M-M0)
+
+    S2 = S2_p + S2_mod + S2_sign
+    err = np.sqrt(err_p**2 + err_mod**2 + err_sign**2)
+
+    return boundaries, np.vstack((S2, err)).T, np.vstack((S2_p, err_p)).T, np.vstack((S2_mod, err_mod)).T, np.vstack((S2_sign, err_sign)).T
+
+
 def SaveConfig(state: str, SWAP_term: str, Ne: np.uint8, Ns: np.uint16,
                Lx: np.float64, Ly: np.float64, t: np.complex128,
                step_size: np.float64, region_geometry: str, boundary: np.float64,
